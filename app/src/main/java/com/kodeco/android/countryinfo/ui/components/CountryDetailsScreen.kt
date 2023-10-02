dba4ffff0f0abee298966586b6cce691a3214d3d
itsmekoko <cocotamarian@yahoo.com>
2023-10-02 15:08:24 +0300

    Aesthetic changes in Detail Screen

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetailsScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryDetailsScreen.kt
@@ -1,10 +1,14 @@
 package com.kodeco.android.countryinfo.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -14,6 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
@@ -37,27 +43,44 @@ fun CountryDetailsScreen(navBackStackEntry: NavBackStackEntry, countries: List<C
             imageVector = Icons.Default.ArrowBack,
             contentDescription = "Back",
             modifier = Modifier
-                .size(48.dp)
+                .size(36.dp)
                 .clickable { navController.navigateUp() },
             tint = Color.Black
         )
 
         // Displaying the Country Details
         country?.let {
-            Text(text = it.commonName, style = MaterialTheme.typography.headlineMedium)
-            Image(
-                painter = rememberAsyncImagePainter(model = it.flags.png),
-                contentDescription = null,
-                modifier = Modifier.size(128.dp)
-            )
-            Text(
-                text = "Capital: ${it.capital?.joinToString() ?: "N/A"}",
-                style = MaterialTheme.typography.bodyLarge
-            )
-            Text(text = "Population: ${it.population}", style = MaterialTheme.typography.bodyLarge)
-            Text(text = "Area: ${it.area} sq km", style = MaterialTheme.typography.bodyLarge)
-        } ?: run {
-            Text("Country details are not available")
+                Text(text = it.commonName, style = MaterialTheme.typography.headlineMedium)
+
+                val aspectRatio = 1.5f
+                Image(
+                    painter = rememberAsyncImagePainter(model = it.flags.png),
+                    contentDescription = null,
+                    modifier = Modifier.width(192.dp)
+                        .padding(top = 16.dp)
+                        .aspectRatio(ratio = aspectRatio)
+                        .border(
+                            BorderStroke(2.dp, Color.Black),
+                            shape = RectangleShape
+
+                        ),
+                    contentScale = ContentScale.FillBounds
+
+                )
+
+                Text(
+                    text = "Capital: ${it.capital?.joinToString() ?: "N/A"}",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+                Text(
+                    text = "Population: ${it.population}",
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(text = "Area: ${it.area} sq km", style = MaterialTheme.typography.titleLarge)
+            } ?: run {
+                Text("Country details are not available")
+            }
         }
     }
-}
+
