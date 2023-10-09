6ab54a24345e13ad9d4963bcd8955534a77b8ce4
itsmekoko <cocotamarian@yahoo.com>
2023-10-09 19:08:42 +0300

    changed retry button color

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryErrorScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryErrorScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,7 +28,7 @@ fun CountryErrorScreen(error: Throwable, onRetry: () -> Unit) {
         Button(onClick = onRetry) {
             androidx.compose.material.Text(
                 text = "Retry",
-                color = if (isSystemInDarkTheme()) Color.White else Color.Black
+                color = MaterialTheme.colors.onPrimary
             )
         }
     }
